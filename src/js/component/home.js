c31e10a25eb0eea5f5f60a7d05a06de57b43a993
yeizmendiz <yahieizmendiz@gmail.com>
2020-10-12 17:43:50 +0000

    Landing page with react

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,65 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
+export const Navbar = props => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
+		<li className="navbar-brand">
+			<a className="navbar-brand" href={props.linkUrl}>
+				{props.link}
 			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		</li>
+	);
+};
+
+Navbar.propTypes = {
+	linkUrl: PropTypes.string,
+	link: PropTypes.string
+};
+
+export const Jumbotron = props => {
+	return (
+		<div className="jumbotron">
+			<h1 className="display-4">{props.title}</h1>
+			<p className="lead">{props.description}</p>
+			<a
+				className="btn btn-primary btn-lg"
+				href={props.buttonURL}
+				role="button">
+				{props.buttonLabel}
+			</a>
 		</div>
 	);
-}
+};
+
+Jumbotron.propTypes = {
+	title: PropTypes.string,
+	description: PropTypes.string,
+	buttonLabel: PropTypes.string,
+	buttonURL: PropTypes.string
+};
+
+export const Card = props => {
+	return (
+		<div className="col-3">
+			<div className="card">
+				<img src={props.cardUrl} className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">{props.cardTitle}</h5>
+					<p className="card-text">{props.cardText}</p>
+					<a href={props.cardButtonUrl} className="btn btn-primary">
+						{props.cardButtonLabel}
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	cardUrl: PropTypes.string,
+	cardTitle: PropTypes.string,
+	cardText: PropTypes.string,
+	cardButtonUrl: PropTypes.string,
+	cardButtonLabel: PropTypes.string
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,76 @@
-//import react into the bundle
+// //include bootstrap npm library into the bundle
+// import "bootstrap";
+
+// //include your index.scss file into the bundle
+// import "../styles/index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-//include bootstrap npm library into the bundle
-import "bootstrap";
+import { Navbar, Jumbotron, Card } from "./component/home";
 
-//include your index.scss file into the bundle
-import "../styles/index.scss";
+const Menu = props => {
+	return (
+		<nav className="navbar navbar-dark bg-dark">
+			<span className="navbar-brand mb-0 h1">{props.logo}</span>
+			<ul className="navbar-brand justify-content-end">
+				<Navbar link="Home" />
+				<Navbar link="About" />
+				<Navbar link="Services" />
+				<Navbar link="Contact" />
+			</ul>
+		</nav>
+	);
+};
 
-//import your own components
-import { Home } from "./component/home.js";
+Menu.propTypes = {
+	logo: PropTypes.string
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const Grid = props => {
+	return (
+		<div className="row text-center">
+			<Card
+				cardUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ-wjupuQMNeJscpw6i3BBzPjMYKsVFX7X0tg&usqp=CAU"
+				cardTitle="Card Title"
+				cardText="ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+				cardButtonLabel="Find Out More"
+			/>
+			<Card
+				cardUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ-wjupuQMNeJscpw6i3BBzPjMYKsVFX7X0tg&usqp=CAU"
+				cardTitle="Card Title"
+				cardText="ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+				cardButtonLabel="Find Out More"
+			/>
+			<Card
+				cardUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxMoWsF10May-tDA9Cuxa09QFnpp7a0FLQuA&usqp=CAU"
+				cardTitle="Card Title"
+				cardText="ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+				cardButtonLabel="Find Out More"
+			/>
+			<Card
+				cardUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ-wjupuQMNeJscpw6i3BBzPjMYKsVFX7X0tg&usqp=CAU"
+				cardTitle="Card Title"
+				cardText="ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+				cardButtonLabel="Find Out More"
+			/>
+		</div>
+	);
+};
+
+ReactDOM.render(
+	<div>
+		<Menu logo="Start Bootstrap" />
+		<div className="container">
+			<Jumbotron
+				title="A Warm Welcome"
+				description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+				buttonLabel="Call To Action!"
+				buttonURL=""
+			/>
+			<Grid />
+		</div>
+	</div>,
+	document.querySelector("#app")
+);
